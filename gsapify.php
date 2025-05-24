64c240051ee97c5bb474680004f76a0f2f2667af
jptweb <jptkts@gmail.com>
2025-05-23 21:26:24 -0400

    Adjusting name of author and new zip

--- gsapify.php
+++ gsapify.php
@@ -5,7 +5,7 @@
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:          0.1.0
- * Author:           Your Name
+ * Author:           John-Paul Takats
  * License:          GPL-2.0-or-later
  * License URI:      https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:      gsapify
